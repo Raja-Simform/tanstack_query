e5f8e3fcb279ee48cef57045beb0d358dc77ae62
Raja-Simform <raja.jha@simformsolutions.com>
2025-06-04 16:25:04 +0530

    feat:add tanstackquery

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,30 +1,35 @@
-import useFetch from "../../hooks/useFetch";
+//import useFetch from "../../hooks/useFetch";
 import { apiPaths } from "../../constants/apiPath";
 import { axiosInstance } from "../../config/axios.config";
 import Card from "../../components/Card/Card";
-import type ApiResponse from "./HomeType";
+//import type ApiResponse from "./HomeType";
 import Filter from "../../components/Filter/Filter";
 import { useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
 import NotFound from "../../components/NotFound/NotFound";
+import { useQuery } from "@tanstack/react-query";
+import type { UsersAPIResponse } from "./HomeType";
 
 export default function Home() {
   const [searchData] = useSearchParams();
-  const search = searchData.get("search");
-  const { data, isLoading, error, refetch } = useFetch({
-    fn: (searchData): Promise<ApiResponse> => {
-      return axiosInstance
-        .get<ApiResponse>(apiPaths.user, { params: { search: searchData } })
-        .then((res) => res.data);
-    },
-  });
+  const search = searchData.get("search") || "";
+  const { data, isLoading, error, refetch } = useQuery<UsersAPIResponse, Error>(
+    {
+      queryKey: ["users", search],
+      queryFn: () => {
+        return axiosInstance
+          .get(apiPaths.user, { params: { search: searchData } })
+          .then((res) => res.data);
+      },
+    }
+  );
   useEffect(() => {
-    refetch(search ?? "");
+    refetch();
   }, [refetch, search]);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
-  if (!data) return <NotFound/>;
+  if (!data) return <NotFound />;
 
   return (
     <div className="flex ">
--- src/pages/Home/HomeType.tsx
+++ src/pages/Home/HomeType.tsx
@@ -5,7 +5,11 @@ interface Product {
   mobileNumber: string;
   createdAt: string;
 }
-
+export interface UsersAPIResponse {
+  status: boolean;
+  message: string;
+  data: Product[];
+}
 export default interface ApiResponse {
   status: boolean;
   message: string;
